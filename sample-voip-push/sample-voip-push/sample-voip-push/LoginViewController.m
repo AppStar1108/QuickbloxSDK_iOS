2638b9a61746b56a1107ae20141adcf1390939ed
Vitaliy G <vitaliy.gurkovsky@injoit.com>
2016-06-07 11:27:40 +0300

    created Sample Voip Push

---
+++ sample-voip-push/sample-voip-push/sample-voip-push/LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  sample-voip-push
+//
+//  Created by Vitaliy Gurkovsky on 6/6/16.
+//  Copyright © 2016 quickblox. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
